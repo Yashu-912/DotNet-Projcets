2b32603acb7ff1768cde576d5d3cac4b7fedb800
Yashu <yash22.gh@gmail.com>
2022-01-25 15:38:41 +0530

    Change in Basic Tutorial

--- 1. BasicTutorial/Program.cs
+++ 1. BasicTutorial/Program.cs
@@ -1,10 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Hello World!");
+Console.WriteLine("Hello Yash!");
+
+string name = "Yash";
+
+Console.WriteLine("Hello " + name);
+Console.WriteLine($"Hello {name}");
+Console.WriteLine($"Hello {name.ToUpper()}");
+
+
 var names = new[] {"Ana", "Felipe", "Emilia"};
 
-foreach(var name in names) {
+foreach(var myname in names) {
 
-    Console.WriteLine($"Hello {name.ToUpper()}!");
-    Console.WriteLine("\n You are trash bro");
+    Console.WriteLine($"Hello {myname.ToUpper()}!");
     
 }
