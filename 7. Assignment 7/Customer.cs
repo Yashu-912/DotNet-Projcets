fc99112f9db987b78896e3ffcf48cd11ccc62b50
Yashu <yash22.gh@gmail.com>
2022-05-05 09:07:38 +0530

    Latest commit

--- 7. Assignment 7/Customer.cs
+++ 7. Assignment 7/Customer.cs
@@ -1,8 +1,8 @@
 public class Customer {
 
-    public string Customerid { get; set; }
+    public string? Customerid { get; set; }
 
-    public string City { get; set; }
+    public string? City { get; set; }
 
     public override string ToString()
     {
